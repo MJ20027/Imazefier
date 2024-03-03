6d2d8de06503e1f9abf82e7bf6f2f86de24419b3
MJ20027 <mi02code@gmail.com>
2024-03-03 10:45:26 +0530

    fill commit

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -34,12 +34,16 @@ import { AspectRatioKey, debounce, deepMergeObjects } from "@/lib/utils";
 import { updateCredits } from "@/lib/actions/user.actions";
 import MediaUploader from "./MediaUploader";
 import TransformedImage from "./TransformedImage";
+import { getCldImageUrl } from "next-cloudinary";
+import { addImage, updateImage } from "@/lib/actions/image.actions";
+import { useRouter } from "next/navigation";
 
 export const formSchema = z.object({
   title: z.string(),
   aspectRatio: z.string().optional(),
   prompt: z.string().optional(),
   publicId: z.string(),
+  color:z.string()
 });
 
 const TransformationForm = ({
@@ -59,7 +63,7 @@ const TransformationForm = ({
   const [isTransforming, setIsTransforming] = useState(false);
   const [transformationConfig, setTransformationConfig] = useState(config);
   const [isPending, startTransition] = useTransition();
-
+  const router = useRouter();
   const initialValues =
     data && action === "Update"
       ? {
@@ -77,8 +81,72 @@ const TransformationForm = ({
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsSubmitting(true);
+    if(data || image){
+      const transformationUrl = getCldImageUrl({
+        width:image?.width,
+        height:image?.height,
+        src: image?.publicId,
+        ...transformationConfig
+      })
+
+      const imageData = {
+        title:values.title,
+        publicId:image?.publicId,
+        transformationType: type,
+        width:image?.width,
+        height:image?.height,
+        config:transformationConfig,
+        secureURL:image?.secureURL,
+        transformationURL:transformationUrl,
+        aspectRatio:values.aspectRatio,
+        prompt:values.prompt,
+        color:values.color,
+    
+      }
+
+      if(action === 'Add'){
+        try {
+          const newImage = await addImage({
+            image:imageData,
+            userId,
+            path:'/'
+          })
+
+          if(newImage){
+            form.reset();
+            setImage(data)
+            router.push(`/transformations/${newImage._id}`)
+
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
+
+      if(action ==='Update'){
+        try {
+          const updatedImage = await updateImage({
+            image: {
+              ...imageData,
+              _id:data._id
+            },
+            userId,
+            path: `/transformations/${data._id}`,
+          });
+
+          if (updatedImage) {
+
+            router.push(`/transformations/${updatedImage._id}`);
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+
+    setIsSubmitting(false);
   }
 
 
@@ -197,9 +265,9 @@ const TransformationForm = ({
             {type === "recolor" && (
               <CustomField
                 control={form.control}
-                // name="color"
+                name="color"
 
-                name="title"
+                // name="title"
                 formLabel="Replacement Color"
                 className="w-full"
                 render={({ field }) => (
